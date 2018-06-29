4202bae83b5e1bd127bd726ba8e299a77c09cc81
themaninorange <jasargent1013@gmail.com>
2018-06-29 18:57:37 -0400

    Get tkinter working with some version of cell activity

--- conway.py
+++ conway.py
@@ -5,6 +5,10 @@ import tkinter as tk
 import time
 
 
+def drawCell(e, color, cellx, celly, cellsizex = 100, cellsizey = 100):
+    e.create_rectangle(cellx, celly, cellx + cellsizex,  celly + cellsizey, fill = color)
+    #e.create_rectangle(cellx, cellx + cellsizex, celly, celly + cellsizey, fill = color)
+    return 0
 
 def stopProg(e):
     root.destroy()
@@ -20,25 +24,34 @@ def drawGrid(e, gapx = 100, gapy = 100, sizex =1000, sizey = 1000):
 
     return 0
 
-def drawCell(e, color, cellx, celly, cellsizex = 100, cellsizey = 100):
-    e.create_rectangle(cellx, cellx + cellsizex, celly, celly + cellsizey, fill = color)
-    return 0
 
-def update():
+def advanceState():
+    global i
     if i == 0:
         i = 1
     else:
         i = 0
 
+def drawCanvas(canvas):
+    global i
+    drawGrid(canvas)
     drawCell(canvas, "white" if i == 1 else "blue" , 200, 200)
+    print("${0} during drawCanvas".format(i))
+
+def update():
+    advanceState()
+    drawCanvas(canvas)
+    print("${0} during update".format(i))
+    root.after(1000,update)
 
 def main():
-    drawGrid(canvas)
     update()
+    print("${0} in main".format(i))
     root.mainloop()
 
 #Globals
 i = 0
+print("${0} after declaration".format(i))
 root = tk.Tk()
 button1 = tk.Button(root, text = "Taxation is Theft. click to close.")
 canvas = tk.Canvas(root, width=500, height = 500, bg = 'white')
