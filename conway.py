e0b03dedbfd1fc4e0be58a6265fd634f2cbc6699
themaninorange <jasargent1013@gmail.com>
2020-02-04 22:19:44 -0500

    Finalized forward conway for now.

--- conway.py
+++ conway.py
@@ -31,10 +31,9 @@ class Ecosystem:
         neighborhood = Ecosystem.neighbors(cells)
         return frozenset().union(Ecosystem.spawn(neighborhood), Ecosystem.survivors(cells, neighborhood))
     
-    def __init__(self, canvas, starterPopulation = frozenset(), cellSize = 100, color = "blue", canvasSize = 1000):
+    def __init__(self, canvas, starterPopulation = frozenset(), cellSize = 100, color = "blue"):
         self.canvas = canvas
         self.cellSize = cellSize
-        self.canvasSize = canvasSize
         self.color = "blue"
         self.livingCells = starterPopulation
         self.neighborhood = self.neighbors(livingCells)
@@ -43,19 +42,19 @@ class Ecosystem:
         self.canvas.after(1, self.draw)  #(time_delay, method_to_execute)
 
 
-    def drawCell(self, color, cellx, celly, cellSizex = 100, cellSizey = 100):
-        self.canvas.create_rectangle(cellx, celly, cellx + cellSizex,  celly + cellSizey, fill = color)
+    def drawCell(self, color, cellx, celly):
+        self.canvas.create_rectangle(cellx, celly, cellx + self.cellSize,  celly + self.cellSize, fill = color)
         #e.create_rectangle(cellx, cellx + cellsizex, celly, celly + cellsizey, fill = color)
         return 0
     
     def drawGrid(self):
         #Verticals
-        for i in range(0, self.canvasSize, self.cellSize):
-            self.canvas.create_line([(i, 0), (i,self.canvasSize)], tag = 'grid_line')
+        for i in range(0, self.canvas.winfo_width(), self.cellSize):
+            self.canvas.create_line([(i, 0), (i,self.canvas.winfo_height())], tag = 'grid_line')
     
         #Horizontals
-        for i in range(0, self.canvasSize, self.cellSize):
-            self.canvas.create_line([(0, i), (self.canvasSize, i)], tag = 'grid_line')
+        for i in range(0, self.canvas.winfo_height(), self.cellSize):
+            self.canvas.create_line([(0, i), (self.canvas.winfo_width(), i)], tag = 'grid_line')
     
         return 0
     
@@ -63,7 +62,7 @@ class Ecosystem:
         self.canvas.delete('all')
         self.drawGrid()
         for cell in list(self.livingCells):
-            self.drawCell("blue" , 100*cell[0], 100*cell[1])
+            self.drawCell("blue" , self.cellSize*cell[0], self.cellSize*cell[1])
         #print("${0} during drawCanvas".format(i))
         return 0
     
@@ -75,7 +74,7 @@ def update():
     esys.drawCanvas()
     esys.generate()
     
-    root.after(1000,update)
+    root.after(10,update)
     
     return 0
 
@@ -98,19 +97,21 @@ i = 0
 
 #livingCells = {(1,1), (1,2), (1, 3)} #spinner
 #livingCells = {(0,0), (1,1), (1, 0), (0, 1)} #box
-livingCells = frozenset({(0,2), (0,1), (1, 2), (2, 0), (2,1), (2,2)}) #lower-case r
+#livingCells = frozenset({(0,2), (0,1), (1, 2), (2, 0), (2,1), (2,2)}) #lower-case r
+livingCells = frozenset({(50,52), (50,51), (51, 52), (52, 50), (52,51), (52,52)}) #lower-case r
 
 root = tk.Tk()
 button1 = tk.Button(root, text = "Taxation is Theft. click to close.")
 canvas = tk.Canvas(root, width=1000, height = 1000, bg = 'white')
 
 
-esys = Ecosystem(canvas, livingCells)
+esys = Ecosystem(canvas, livingCells, cellSize = 4)
 
 #Button seems to be broken.  "name 'root' is not defined"
 button1.pack()
 button1.bind('<Button-1>', stopProg)
 canvas.pack()
+
 
 if __name__ == "__main__":
     main()
