ca30bc2352d7f4ebe279af2b51774a8de2cd646b
themaninorange <jasargent1013@gmail.com>
2020-02-04 21:52:06 -0500

    Fixed Grid issue

--- conway.py
+++ conway.py
@@ -1,92 +1,88 @@
-# Conway's game of life, forward.
-# After all, we need to test this.
+#!/usr/bin/python
 
-import tkinter as tk
+"""Conway's game of life, forward.  After all, we need to test this.
+"""
+
+import Tkinter as tk
 import time
 import collections as col
 
 class Ecosystem:
-    def __init__(self, canvas, starterPopuation, cellsize = 100, color = "blue"):
+    @staticmethod
+    def cellNeighbors(c):
+        return [(c[0]-1, c[1]-1), (c[0]  , c[1]-1), (c[0]+1, c[1]-1),  # Shaped like this: n n n
+                (c[0]-1, c[1]  ),                   (c[0]+1, c[1]  ),  #                   n   n
+                (c[0]-1, c[1]+1), (c[0]  , c[1]+1), (c[0]+1, c[1]+1)]  #                   n n n
+    
+    @staticmethod
+    def neighbors(cells):
+        return col.Counter([x for y in [Ecosystem.cellNeighbors(cell) for cell in list(cells)] for x in y])
+    
+    @staticmethod
+    def spawn(neighborhood):
+        return frozenset([key for key, value in neighborhood.items() if value == 3])
+    
+    @staticmethod
+    def survivors(cells, neighborhood):
+        return frozenset([key for key, value in neighborhood.items() if value == 2]).intersection(cells)
+    
+    @staticmethod
+    def nextGeneration(cells):
+        neighborhood = Ecosystem.neighbors(cells)
+        return frozenset().union(Ecosystem.spawn(neighborhood), Ecosystem.survivors(cells, neighborhood))
+    
+    def __init__(self, canvas, starterPopulation = frozenset(), cellSize = 100, color = "blue", canvasSize = 1000):
         self.canvas = canvas
-        self.cellsize = cellsize
+        self.cellSize = cellSize
+        self.canvasSize = canvasSize
         self.color = "blue"
         self.livingCells = starterPopulation
+        self.neighborhood = self.neighbors(livingCells)
 
     def draw(self):
         self.canvas.after(1, self.draw)  #(time_delay, method_to_execute)
 
 
-def drawCell(e, color, cellx, celly, cellsizex = 100, cellsizey = 100):
-    e.create_rectangle(cellx, celly, cellx + cellsizex,  celly + cellsizey, fill = color)
-    #e.create_rectangle(cellx, cellx + cellsizex, celly, celly + cellsizey, fill = color)
+    def drawCell(self, color, cellx, celly, cellSizex = 100, cellSizey = 100):
+        self.canvas.create_rectangle(cellx, celly, cellx + cellSizex,  celly + cellSizey, fill = color)
+        #e.create_rectangle(cellx, cellx + cellsizex, celly, celly + cellsizey, fill = color)
+        return 0
+    
+    def drawGrid(self):
+        #Verticals
+        for i in range(0, self.canvasSize, self.cellSize):
+            self.canvas.create_line([(i, 0), (i,self.canvasSize)], tag = 'grid_line')
+    
+        #Horizontals
+        for i in range(0, self.canvasSize, self.cellSize):
+            self.canvas.create_line([(0, i), (self.canvasSize, i)], tag = 'grid_line')
+    
+        return 0
+    
+    def drawCanvas(self):
+        self.canvas.delete('all')
+        self.drawGrid()
+        for cell in list(self.livingCells):
+            self.drawCell("blue" , 100*cell[0], 100*cell[1])
+        #print("${0} during drawCanvas".format(i))
+        return 0
+    
+    def generate(self):
+        self.livingCells = Ecosystem.nextGeneration(self.livingCells)
+    
+def update():
+    
+    esys.drawCanvas()
+    esys.generate()
+    
+    root.after(1000,update)
+    
     return 0
+
 
 def stopProg(e):
     root.destroy()
-
-def drawGrid(e, gapx = 100, gapy = 100, sizex =1000, sizey = 1000):
-    #Verticals
-    for i in range(0, sizex, gapx):
-        e.create_line([(i, 0), (i,sizex)], tag = 'grid_line')
-
-    #Horizontals
-    for i in range(0, sizex, gapx):
-        e.create_line([(0, i), (sizey, i)], tag = 'grid_line')
-
     return 0
-
-def cellNeighbors(c):
-    return [(c[0]-1, c[1]-1), (c[0]  , c[1]-1), (c[0]+1, c[1]-1),  # Shaped like this: n n n
-            (c[0]-1, c[1]  ),                   (c[0]+1, c[1]  ),  #                   n   n
-            (c[0]-1, c[1]+1), (c[0]  , c[1]+1), (c[0]+1, c[1]+1)]  #                   n n n
-
-def neighbors(cells):
-    return col.Counter([x for y in [cellNeighbors(cell) for cell in list(cells)] for x in y])
-
-def spawn(neighborhood):
-    return frozenset([key for key, value in neighborhood.items() if value == 3])
-
-def survivors(cells, neighborhood):
-    return frozenset([key for key, value in neighborhood.items() if value == 2]).intersection(cells)
-
-def nextGeneration(cells):
-    neighborhood = neighbors(cells)
-    return frozenset().union(spawn(neighborhood), survivors(cells, neighborhood))
-
-def drawCanvas(canvas):
-    global i
-    drawGrid(canvas)
-    for cell in list(livingCells):
-        drawCell(canvas, "blue" , 100*cell[0], 100*cell[1])
-    #print("${0} during drawCanvas".format(i))
-
-def update():
-    
-    drawCanvas(canvas)
-    
-    livingCells = nextGeneration
-    root.after(1000,update)
-
-def findNewLife():
-    neighborToLife = set([*[(x-1, y-1), (x-1, y), (x-1, y+1), \
-                            (x,   y-1),           (x,   y+1), \
-                            (x+1, y-1), (x+1, y), (x+1, y+1)] for x, y in livingCells])
-    # ^ This is broken.  I want to find all cells adjacent to living cells
-    # I will use that list, once made, to check all of the dead cells to see if
-    #    they come [back] to life.
-        pass
-    setx = set([point[0] for point in livingCells])
-    sety = set([point[1] for point in livingCells])
-    locationLookupx = {x: set([point for point in livingCells if abs(point[0] - x) <= 1]) for x in setx}
-    locationLookupy = {y: set([point for point in livingCells if abs(point[1] - y) <= 1]) for y in sety}
-
-    # Now we want to find the intersection.
-    locationLookupFull = {(x,y): locationLookupx[x].intersection(locationLookupy[y]) for x in setx for y in sety}
-
-    #
-
-
-
 
 
 def main():
@@ -94,28 +90,27 @@ def main():
     print('LIFE: ' + str(livingCells))
     #print("${0} in main".format(i))
     root.mainloop()
+    
+    return 0
 
 #Globals
 i = 0
 
-#livingCells = {(0,0), (1,1), (1, 0), (0, 1)}
-#livingCells = frozenset({(0,2), (0,1), (1, 2), (2, 0), (2,1), (2,2)})
+#livingCells = {(1,1), (1,2), (1, 3)} #spinner
+#livingCells = {(0,0), (1,1), (1, 0), (0, 1)} #box
+livingCells = frozenset({(0,2), (0,1), (1, 2), (2, 0), (2,1), (2,2)}) #lower-case r
 
-livingCells = {(0,0), (1,1), (1, 0), (0, 1)} #type: set
-knownDead = set()
-#cellSize =
-#print("${0} after declaration".format(i))
 root = tk.Tk()
 button1 = tk.Button(root, text = "Taxation is Theft. click to close.")
-canvas = tk.Canvas(root, width=500, height = 500, bg = 'white')
+canvas = tk.Canvas(root, width=1000, height = 1000, bg = 'white')
+
+
+esys = Ecosystem(canvas, livingCells)
 
 #Button seems to be broken.  "name 'root' is not defined"
 button1.pack()
 button1.bind('<Button-1>', stopProg)
 canvas.pack()
-
-
-
 
 if __name__ == "__main__":
     main()
